87fb8b2d841297a8b4abdf6768bf16bb1e52b020
Surbhi Oberoi <siyaoberoi93@gmail.com>
2016-04-13 02:40:04 +0530

    Creating functions for sum, remove and remove without copy.

--- app/arrays.js
+++ app/arrays.js
@@ -13,15 +13,41 @@ exports.arraysAnswers = {
   },
 
   sum : function(arr) {
+    var result = 0;
+    for (var i = 0; i < arr.length; i++) {
+      result += arr[i];
+    }
+    return result;
 
   },
 
   remove : function(arr, item) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+      if (!(arr[i] === item)){
+        result.push(arr[i]);
+      }
+
+    }
+
+    return result;
 
   },
 
   removeWithoutCopy : function(arr, item) {
+    var indexOfItems = [];
 
+    for (var i in arr) {
+      if (arr[i] === item) {
+        indexOfItems.push(i);
+      }
+    }
+
+    for (var i in indexOfItems) {
+      arr.splice(indexOfItems[i]-i, 1);
+    }
+
+    return arr;
   },
 
   append : function(arr, item) {
